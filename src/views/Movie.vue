ab12fa40ea685a381df810969c065262bdd2a086
Casey Justus <cjustus@keyholesoftware.com>
2018-11-19 10:16:00 -0600

    removed title and release date to match angular app

--- src/views/Movie.vue
+++ src/views/Movie.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="movie-detail">
-    <h2>{{movie.title}}</h2>
-    <p>Released on {{movie.release_date}}</p>
+
     <h3>{{movie.overview}}</h3>
     <a href="/#/movies"><span class="close"></span></a>
     <img :src="'http://localhost:8080/images/backdrops' + movie.backdrop_path"/>
