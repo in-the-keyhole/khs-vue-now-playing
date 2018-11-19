db5128fd7bb0da612032a542ee2b7d8b23931888
Casey Justus <cjustus@keyholesoftware.com>
2018-11-19 14:57:28 -0600

    cleanup

--- src/router.js
+++ src/router.js
@@ -2,27 +2,36 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Movies from './views/Movies.vue'
 import Movie from './views/Movie.vue'
+//import Login from './views/Login.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'movies',
-      component: Movies,
+        path: '/',
+        name: 'movies',
+        component: Movies,
     },
     {
-      path: '/movies',
-      name: 'movies',
-      component: Movies
+        path: '/movies',
+        name: 'movies',
+        component: Movies
     },
     {
-      path: '/movie/:id',
-      name: 'movie',
-      component: Movie,
-      props: true
-    }
+        path: '/movie/:id',
+        name: 'movie',
+        component: Movie,
+        props: true
+    },
+    /*
+    {
+        path: '/login',
+        name: 'login',
+        component: Movie,
+        props: true
+    },
+  */
 
 
 
